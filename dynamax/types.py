0c3995a5175aaf752d9867757788560db3a738e2
andrewlesak <122942833+andrewlesak@users.noreply.github.com>
2024-12-11 19:55:22 -0800

    update deprecated typing code

--- dynamax/types.py
+++ dynamax/types.py
@@ -2,8 +2,9 @@ from typing import Optional, Union
 from typing_extensions import Protocol
 from jaxtyping import Array, Float
 import jax._src.random as prng
+import jax
 
-
-PRNGKey = prng.KeyArray
+# PRNGKey = prng.KeyArray  # deprecated in jax version 0.4.16
+PRNGKey = jax.Array  
 
 Scalar = Union[float, Float[Array, ""]] # python float or scalar jax device array with dtype float
